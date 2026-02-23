6fb8c3a73a1782f35fb42913ea348a1b4c98ec04
Rima Falko <rima@falko.email>
2026-02-23 13:08:00 +0100

    do key-value-pair as record

--- src/main/java/com/osuplayer/common/KeyValuePair.java
+++ src/main/java/com/osuplayer/common/KeyValuePair.java
@@ -1,19 +1,3 @@
 package com.osuplayer.common;
 
-public class KeyValuePair<Key, Value> {
-    private final Key key;
-    private final Value value;
-
-    public KeyValuePair(Key key, Value value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public Key key() {
-        return key;
-    }
-
-    public Value value() {
-        return value;
-    }
-}
+public record KeyValuePair<Key, Value>(Key key, Value value) { }
